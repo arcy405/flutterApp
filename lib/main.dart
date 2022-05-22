8c7b2fdd988d53b9d32a6085a00323895c3be26b
Archana Pant <arcy405@gmail.com>
2022-05-22 16:14:26 +0545

    day 3 completes

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_catalog/home_page.dart';
+import 'package:flutter_catalog/pages/home_page.dart';
+import 'package:flutter_catalog/pages/login_page.dart';
 import 'package:matcher/matcher.dart';
 
 void main() {
@@ -13,7 +14,15 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: HomePage(),
+      themeMode: ThemeMode.light,
+      theme: ThemeData(primarySwatch: Colors.lime),
+      darkTheme: ThemeData(primarySwatch: Colors.red),
+      initialRoute: "/home",
+      routes: {
+        '/': (context) => LoginPage(),
+        '/home': (context) => HomePage(),
+        '/login': (context) => LoginPage(),
+      },
     );
   }
 }
