093d040a7913df8f87aee8f11ad7029734fe002d
Archana Pant <arcy405@gmail.com>
2022-05-26 13:01:45 +0545

    fg

---
+++ lib/widgets/item_widget.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+import '../models/catalog.dart';
+
+class ItemWidget extends StatelessWidget {
+  final Item item;
+
+  const ItemWidget({super.key, required this.item});
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: ListTile(
+        onTap: () {
+          print("${item.name} tapped!");
+        },
+        leading: Image.network(item.image),
+        title: Text(item.name),
+        subtitle: Text(item.desc),
+        trailing: Text(
+          '\$${item.price}',
+          textScaleFactor: 1.2,
+          style: const TextStyle(
+            fontWeight: FontWeight.bold,
+            color: Colors.deepPurple,
+          ),
+        ),
+      ),
+    );
+  }
+}
