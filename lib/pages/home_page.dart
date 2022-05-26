8a5850cda9235de77d79adf4fc0d8158a77f0406
Archana Pant <arcy405@gmail.com>
2022-05-26 14:01:24 +0545

    vd

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,14 +1,38 @@
-import 'package:flutter/material.dart';
-import 'package:flutter_catalog/models/catalog.dart';
+// ignore_for_file: avoid_print
 
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_catalog/models/catalog.dart';
+import 'dart:convert';
 import '../widgets/drawer.dart';
 import '../widgets/item_widget.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
 
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
   final days = 97;
+
   final name = 'Flutter';
+
+  @override
+  // ignore: must_call_super
+  void initState() {
+    // TODO: implement initState
+    loadData();
+  }
+
+  loadData() async {
+    final catalogJson =
+        await rootBundle.loadString("assets/files/catalog.json");
+    final decodedData = jsonDecode(catalogJson);
+    var productsData = decodedData["products"];
+    print(productsData);
+  }
 
   @override
   Widget build(BuildContext context) {
